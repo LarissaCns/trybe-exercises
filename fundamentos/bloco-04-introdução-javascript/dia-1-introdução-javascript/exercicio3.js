91a345ad33f21295fc0472d07f9623fb8212cb22
Larissa Cristina <larissacristina.nunes@outlook.com>
2021-11-05 19:14:07 -0300

    Adiciona exercicio3.js

---
+++ fundamentos/bloco-04-introdução-javascript/dia-1-introdução-javascript/exercicio3.js
@@ -0,0 +1,9 @@
+const candidata = 59;
+
+if ( candidata >= 80 ){
+    console.log('Parabéns, você foi aprovada(o)!');
+} else if ( candidata < 80 && candidata >= 60 ) {
+    console.log('Você está na nossa lista de espera');
+} else {
+    console.log('Você foi reprovada(o)');
+}
