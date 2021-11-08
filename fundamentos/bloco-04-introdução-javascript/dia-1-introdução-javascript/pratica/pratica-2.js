fd570f90de9a5a453d1382fc5fc7c863325dbd42
Larissa Cristina <larissacristina.nunes@outlook.com>
2021-11-08 10:59:37 -0300

    Adiciona exercícios prática.js

---
+++ fundamentos/bloco-04-introdução-javascript/dia-1-introdução-javascript/pratica/pratica-2.js
@@ -0,0 +1,10 @@
+//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+
+const numb1 = 10;
+const numb2 = 20;
+
+if (numb1 > numb2) {
+    console.log('O número ',numb1,' é o maior !!');
+}else {
+    console.log('O número ',numb2,' é o maior !!')
+}
