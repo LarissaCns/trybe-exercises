d7b59682f1c5639b8ab4c95a7f655049da24d555
Larissa Cristina <larissacristina.nunes@outlook.com>
2022-01-05 22:59:05 -0300

    exercicios

--- fundamentos/bloco-8-higher-order-functions/dia-1-introdução-a-hof/praticas/pratica1.js
+++ fundamentos/bloco-8-higher-order-functions/dia-1-introdução-a-hof/praticas/pratica1.js
@@ -8,3 +8,51 @@ const rotina = (func) => {
 };
 
 rotina(acordei);
+
+//HOF Simples ----------------------
+/* const repeat = (number, action) => { // função contadora 
+    for (let count = 0; count <= number; count += 1) {
+      action(count);
+    }
+  };
+  
+repeat(5, console.log); */
+
+// HOF mais complexa --------------------
+/* const repeat = (number, action) => { // função contadora 
+    for (let count = 0; count <= number; count += 1) {
+      action(count);
+    }
+  };
+  
+  repeat(3, (number) => { //Um número até que ponto gostaríamos de testar, neste caso 3 ;
+    if (number % 2 === 0) { // numeros pares 
+      console.log(number, 'is even');
+    }
+  }); */
+
+//----------------------------
+/* const repeat = (number, action) => {
+    for (let count = 0; count <= number; count += 1) {
+      action(count);
+    }
+  };
+  
+  //callback
+  const isEven = (number) => {
+    if (number % 2 === 0) {
+      console.log(number, 'is even');
+    }
+  };
+  
+  //callback
+  const isOdd = (number) => {
+    if ((number % 2) > 0) {
+      console.log(number, 'is odd');
+    }
+  };
+  
+  repeat(3, isEven); // Testa quais números serão pares;
+  repeat(3, isOdd); // Testa quais números serão ímpares; */
+
+
